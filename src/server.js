7ab5b6502784cd976f0000c5c36bd2683449b0a5
Thiago Gerard <114416962+thiagogerard@users.noreply.github.com>
2025-11-27 15:55:55 -0800

    feat: configure server to use environment variable for port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db.js');
 const devicesRoute = require('./routes/devicesRoute.js');
+require('dotenv').config();
 
 const app = express();
 
@@ -9,7 +10,7 @@ connectDB();
 app.use(express.json());
 app.use(devicesRoute);
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server runnign on port ${PORT}`);
 });
